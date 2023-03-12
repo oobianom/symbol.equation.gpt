fbfa750d7a45030997c9c3582961ff57add51107
Obi Obianom <oobianom@umaryland.edu>
2023-03-12 06:15:00 -0400

    Update handler.R

--- R/handler.R
+++ R/handler.R
@@ -1,14 +1,16 @@
 #' Add symbol to current page
 #'
 #' Add selected symbol to page
+#' @param symbolid the symbol id to include
 #'
 #' @export
 #'
 
-add.Symbol <- function(symbolid) {
+add.Symbol <- function(symbolid,type=c(1:3)) {
   a <- rstudioapi::getSourceEditorContext()
   s <- a$selection
   n <- length(s)
+  add.Symbol <- paste0("sym(",add.Symbol,")")
   # if no text is selected
   if (n == 1L && nchar(s[[1L]]$text) == 0L) {
     pos <- s[[1L]]$range$start
