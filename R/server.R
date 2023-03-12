46a68eb671023719cce5c1bf596e516948578cfd
Obi Obianom <oobianom@umaryland.edu>
2023-03-12 06:12:46 -0400

    Update server.R

--- R/server.R
+++ R/server.R
@@ -43,24 +43,3 @@ r2sym.server <- function(input, output, session) {
 
 
 
-#' Add symbol to current page
-#'
-#' Add selected symbol to page
-#'
-#' @export
-#'
-
-add.Symbol <- function(symbolid) {
-  a <- rstudioapi::getSourceEditorContext()
-  s <- a$selection
-  n <- length(s)
-  # if no text is selected
-  if (n == 1L && nchar(s[[1L]]$text) == 0L) {
-    pos <- s[[1L]]$range$start
-    pos[2L] <- 1
-    rstudioapi::insertText(location = pos, text = symbolid)
-  } else {
-    for (i in s)
-      rstudioapi::insertText(location = i$range$start, text = symbolid)
-  }
-}
