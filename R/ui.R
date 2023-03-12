0fedcdb6851867b99bcd5b1b68e7ce4e7963106b
Obi Obianom <oobianom@umaryland.edu>
2023-03-12 12:40:28 -0400

    Update ui.R

--- R/ui.R
+++ R/ui.R
@@ -16,22 +16,22 @@ lib.loc <- function() file.path(find.package(package = "symbols.equations.ui"), 
 #' @export
 #'
 tab2 <- nextGenShinyApps::tabPanel("Equations", shiny::div(
-  r2symbols::symKey(keyword = "sign", font.color="gray", font.weight = "light",font.size = 32)
+  r2symbols::symKey(keyword = "sign", font.color = "gray", font.weight = "light", font.size = 32)
 ))
 tab3 <- nextGenShinyApps::tabPanel("Symbols", shiny::div(
-  r2symbols::symCat(category = "symbol", font.color="gray", font.weight = "light",font.size = 32)
+  r2symbols::symCat(category = "symbol", font.color = "gray", font.weight = "light", font.size = 32)
 ))
 tab4 <- nextGenShinyApps::tabPanel("Greek Letters", shiny::div(
-  r2symbols::symCat(category = "mixed", font.color="gray", font.weight = "light",font.size = 32)
+  r2symbols::symCat(category = "mixed", font.color = "gray", font.weight = "light", font.size = 32)
 ))
 tab5 <- nextGenShinyApps::tabPanel("Arrows", shiny::div(
-  r2symbols::symCat(category = "arrows", font.color="gray", font.weight = "light",font.size = 32)
+  r2symbols::symCat(category = "arrows", font.color = "gray", font.weight = "light", font.size = 32)
 ))
 tab1 <- nextGenShinyApps::tabPanel("Emojis", shiny::div(
-  r2symbols::symCat(category = "smiley", font.color="gray", font.weight = "light",font.size = 32)
+  r2symbols::symCat(category = "smiley", font.color = "gray", font.weight = "light", font.size = 32)
 ))
 tab0 <- nextGenShinyApps::tabPanel("Home", shiny::div(
-  #shiny::withMathJax(shiny::includeMarkdown(file.path(lib.loc(),"introduction.md")))
+  # shiny::withMathJax(shiny::includeMarkdown(file.path(lib.loc(),"introduction.md")))
   shiny::withMathJax(shiny::includeMarkdown("https://symbols-ui.obi.obianom.com/assets/introduction.md"))
 ))
 
@@ -52,13 +52,19 @@ r2sym.ui <- nextGenShinyApps::fluidPage(
   # Header: Insert header content using titlePanel ------------
   header = nextGenShinyApps::titlePanel(
     left = "Symbols Interface",
-    right = nextGenShinyApps::actionButton("closeapp","",icon = shiny::icon("window-close"),style="pill",bg.type = "primary")),
-shiny::div(
-  shiny::selectInput("sek1","Copy or Insert", choices = c("hello","Boo")),
-shiny::conditionalPanel(
-  condition = "input.sek1 == 'Boo'",
-                 shiny::selectInput("sek2","Select this", choices = c("hello","B"))
-                 ), class = "d-flex"),
+    right = nextGenShinyApps::actionButton("closeapp", "", icon = shiny::icon("window-close"), style = "pill", bg.type = "primary")
+  ),
+  shiny::div(
+    shiny::selectInput("symbolseqnui-copy-insert", "Copy or Insert", choices = c("Copy to Clipboard", "Insert in Document")),
+    shiny::conditionalPanel(
+      condition = "input.symbolseqnui-copy-insert == 'Insert in Document'",
+      div(
+        shiny::selectInput("symbolseqnui-insert-type", "Insert type", choices = c("hello", "B")),
+        class = "pl-2"
+      )
+    ),
+    class = "d-flex"
+  ),
   nextGenShinyApps::tabsetPanel(
 
     tab0,
@@ -71,6 +77,3 @@ shiny::conditionalPanel(
     justified = TRUE
   )
 )
-
-
-
