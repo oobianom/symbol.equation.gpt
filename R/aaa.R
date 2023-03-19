9309f12a7c65c9beb45972120fe75ac87c55ccb2
Obi Obianom <oobianom@umaryland.edu>
2023-03-19 16:22:43 -0400

    Update aaa.R

---
+++ R/aaa.R
@@ -0,0 +1,22 @@
+#' Library initialize and path loader
+#'
+#' Initialize and load library path for package
+#'
+#' @return library location
+#'
+#' @examples
+#' envir.prep()
+#' @export
+#'
+
+(function() {
+  # library calls
+  require(shiny, quietly = TRUE)
+  require(symbol.equation.gpt, quietly = TRUE)
+  require(nextGenShinyApps, quietly = TRUE)
+  require(r2symbols, quietly = TRUE)
+  require(shinyStorePlus, quietly = TRUE)
+
+  # return lib
+  file.path(find.package(package = "symbol.equation.gpt"), "lib")
+}) -> envir.prep
